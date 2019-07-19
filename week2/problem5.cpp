32a5b22e948552e925a1f2dbb5d99b1a8b0129b6
김정우 (JW Kim) <gts2030@snu.ac.kr>
2019-07-19 20:06:16 +0900

    changed name

--- week2/problem5.cpp
+++ week2/problem5.cpp
@@ -6,33 +6,65 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <cmath>
+
 class Solution {
 public:
-	ListNode* partition(ListNode* head, int x) {
-		ListNode* dummy = new ListNode(0);
-		ListNode* ins = NULL;
-		ListNode* tmp;
-
-		dummy->next = head;
-		ListNode* pt = dummy;
-		while (pt->next) {
-			if (pt->next->val >= x) {
-				ins = pt;
-				break;
-			}
-			pt = pt->next;
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode* pt1 = l1;
+		ListNode* pt2 = l2;
+		int n1 = 0, n2 = 0;
+		while (pt1) {
+			n1++;
+			pt1 = pt1->next;
+		}
+		while (pt2) {
+			n2++;
+			pt2 = pt2->next;
+		}
+		if (n1 >= n2) {
+			pt1 = l1;
+			pt2 = l2;
+		}
+		else {
+			pt1 = l2;
+			pt2 = l1;
 		}
 
-		while (pt->next) {
-			if (pt->next->val < x) {
-				tmp = pt->next;
-				pt->next = tmp->next;
-				tmp->next = ins->next;
-				ins->next = tmp;
-				ins = tmp;
+		bool over = false;
+		ListNode* prev;
+		while (pt2) {
+			if (over) {
+				pt1->val++;
+				over = false;
 			}
-			else if (pt->next) pt = pt->next;
+			pt1->val += pt2->val;
+			if (pt1->val > 9) {
+				pt1->val %= 10;
+				over = true;
+			}
+			prev = pt1;
+			pt1 = pt1->next;
+			pt2 = pt2->next;
 		}
-		return dummy->next;
+		if (over) {
+			if (pt1 == NULL) prev->next = new ListNode(1);
+			else {
+				pt1->val++;
+				while (pt1) {
+					if (pt1->val > 9) {
+						pt1->val %= 10;
+						if (pt1->next == NULL) {
+							pt1->next = new ListNode(1);
+							break;
+						}
+						else pt1->next->val++;
+					}
+					pt1 = pt1->next;
+				}
+			}
+		}
+		if (n1 >= n2) return l1;
+		else return l2;
 	}
 };
