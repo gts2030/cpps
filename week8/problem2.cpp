c4c480e1f2468a9f194cd19d9b6e2333ce690a07
Jungwoo Kim <gts2030@snu.ac.kr>
2019-08-30 15:35:06 +0900

    Create problem2.cpp

---
+++ week8/problem2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        set<int> s;
+        for(const auto &num : nums) s.insert(num);
+        int sum = std::accumulate(s.begin(), s.end(), 0);
+        int sum_vec = std::accumulate(nums.begin(), nums.end(), 0);
+        return sum*2 - sum_vec;
+    }
+};
