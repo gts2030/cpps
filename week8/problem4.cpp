bd465011a6c6e89ed46c57d95d381ecc10ae2d15
Jungwoo Kim <gts2030@snu.ac.kr>
2019-08-30 16:10:46 +0900

    Create problem4.cpp

---
+++ week8/problem4.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        int limit = nums.size() / 2;
+        for (const auto &num: nums) {
+            if (++cnt[num] > limit) return num;
+        }
+        return 0;
+    }
+};
